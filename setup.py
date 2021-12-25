d66969300948aa141b9250b99a26663ce5088abb
Ole Spaarmann <os@codeshift.co>
2021-12-25 17:56:45 +0100

    Change name of tensorflow to tensorflow-macos.

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'tensorflow==2.5.0',
+        'tensorflow-macos==2.5.0',
         'pandas>1.2.3'
     ],
 
